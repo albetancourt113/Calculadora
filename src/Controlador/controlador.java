1c56b0e9b829e032794d6584d52cd74a4c009025
albetancourt113 <albertobm545@gmail.com>
2019-12-01 12:12:31 -0600

    implementación de la clase controlador

--- src/Controlador/controlador.java
+++ src/Controlador/controlador.java
@@ -5,10 +5,61 @@
  */
 package Controlador;
 
+import Modelo.InfijaAPosfija;
+import Vista.vistaPrincipal;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  *
  * @author alber
  */
-public class controlador {
+public class controlador implements ActionListener {
+    private InfijaAPosfija modelo;
+    private vistaPrincipal vista;
+
+    public controlador(InfijaAPosfija modelo, vistaPrincipal vista) {
+        this.modelo = modelo;
+        this.vista = vista;
+        this.vista.jButtonSalir.addActionListener(this);
+        this.vista.jButtonLimpiar.addActionListener(this);
+        this.vista.jButtonEvaluar.addActionListener(this);
+    }
     
+    public void iniciar() {
+        vista.setTitle("Vista principal");
+        vista.setLocationRelativeTo(null);
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == vista.jButtonEvaluar) {
+            String posfija = modelo.infijaAPosfija(vista.jTextFieldExpresion.getText());
+            String resultado = modelo.evaluarPosfija(posfija);
+            vista.jTextFieldResultado.setText(resultado);
+        }else if(e.getSource() == vista.jButtonLimpiar){
+            vista.jTextFieldExpresion.setText(null);
+            vista.jTextFieldResultado.setText("");
+        }else if(e.getSource() == vista.jButtonSalir){
+            vista.setVisible(false);
+            vista.dispose();
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
