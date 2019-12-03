2865166b957db7f1f534496ae75661299b5185bf
jonichi <jo.ni1969@hotmail.com>
2019-12-02 19:06:18 -0600

    se agrego comentarios para el javadoc
    se agregaron comentarios para generar el java doc

--- src/Controlador/controlador.java
+++ src/Controlador/controlador.java
@@ -17,9 +17,10 @@ import java.awt.event.ActionListener;
  * @author alber
  */
 public class controlador implements ActionListener {
+
     private InfijaAPosfija modelo;
     private vistaPrincipal vista;
-    
+
     public controlador(InfijaAPosfija modelo, vistaPrincipal vista) {
         this.modelo = modelo;
         this.vista = vista;
@@ -27,62 +28,45 @@ public class controlador implements ActionListener {
         this.vista.jButtonLimpiar.addActionListener(this);
         this.vista.jButtonEvaluar.addActionListener(this);
     }
-    
+
     public void iniciar() {
         vista.setTitle("Vista principal");
         vista.setLocationRelativeTo(null);
     }
     private AnalizadorLexico analizadorLexico = new AnalizadorLexico();
     private AnalizadorSintactico analizadorSintactico = new AnalizadorSintactico();
+
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == vista.jButtonEvaluar) {
-            if(analizadorLexico.ValidarLexico(vista.jTextFieldExpresion.getText())){
-                if(analizadorSintactico.ValidarParentesis(vista.jTextFieldExpresion.getText())){
-                String posfija = modelo.infijaAPosfija(vista.jTextFieldExpresion.getText());
-                  if(analizadorSintactico.validarExpresion(posfija)){
-                      String resultado = modelo.evaluarPosfija(posfija);
-                      vista.jTextFieldResultado.setText(resultado);
-                      vista.jTextFieldObservaciones.setText("");
-                  }else{
-                      vista.jTextFieldResultado.setText("Error sintactico");
-                      vista.jTextFieldObservaciones.setText(analizadorSintactico.getError());
-                  }
-                  
-                }else{
+            if (analizadorLexico.ValidarLexico(vista.jTextFieldExpresion.getText())) {
+                if (analizadorSintactico.ValidarParentesis(vista.jTextFieldExpresion.getText())) {
+                    String posfija = modelo.infijaAPosfija(vista.jTextFieldExpresion.getText());
+                    if (analizadorSintactico.validarExpresion(posfija)) {
+                        String resultado = modelo.evaluarPosfija(posfija);
+                        vista.jTextFieldResultado.setText(resultado);
+                        vista.jTextFieldObservaciones.setText("");
+                    } else {
+                        vista.jTextFieldResultado.setText("Error sintactico");
+                        vista.jTextFieldObservaciones.setText(analizadorSintactico.getError());
+                    }
+
+                } else {
                     vista.jTextFieldResultado.setText("Error sintactico");
                     vista.jTextFieldObservaciones.setText(analizadorSintactico.getError());
                 }
-            }else{
-            vista.jTextFieldResultado.setText("Error lexico");
-            vista.jTextFieldObservaciones.setText(analizadorLexico.getCaracterInvalido());
-                    
+            } else {
+                vista.jTextFieldResultado.setText("Error lexico");
+                vista.jTextFieldObservaciones.setText(analizadorLexico.getCaracterInvalido());
+
             }
-      
-        }else if(e.getSource() == vista.jButtonLimpiar){
+
+        } else if (e.getSource() == vista.jButtonLimpiar) {
             vista.jTextFieldExpresion.setText(null);
             vista.jTextFieldResultado.setText("");
             vista.jTextFieldObservaciones.setText("");
-        }else if(e.getSource() == vista.jButtonSalir){
+        } else if (e.getSource() == vista.jButtonSalir) {
             vista.setVisible(false);
             vista.dispose();
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/Modelo/AnalizadorLexico.java
+++ src/Modelo/AnalizadorLexico.java
@@ -1,72 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Modelo;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- *
- * @author joni1
+ *@author jonsthan chi cuevas   
+ * clase que analiza y valida los caractes de la expresion 
  */
 public class AnalizadorLexico {
-private String Error;
-public final static String DECIMAL = ".";
-public final static int PRIMERCARACTER=1;
 
+    private String Error;
+    public final static String DECIMAL = ".";
+    public final static int PRIMERCARACTER = 1;
 
-enum Tipos {
-        NUMERO ("[0-9]+"),
+    
+    enum Tipos {
+        NUMERO("[0-9]+"),
         OPERADOR_BINARIO("[-|/|+|*|(|)|^| ]");
 
         public final String patron;
+
         Tipos(String s) {
             this.patron = s;
         }
     }
-public boolean ValidarLexico(String input){
+    /**
+     * Metodo que valida los caracteres de una expresion 
+     * @param input expresion a validar
+     * @return parametros validos o invalidos
+     */
+
+    public boolean ValidarLexico(String input) {
         String expresion = input;
         boolean caracterValido = false;
-            for(int posicionCaracter=PRIMERCARACTER;posicionCaracter<expresion.length()+1;posicionCaracter++){
+        for (int posicionCaracter = PRIMERCARACTER; posicionCaracter < expresion.length() + 1; posicionCaracter++) {
             caracterValido = false;
             for (Tipos tokenTipo : Tipos.values()) {
                 Pattern patron = Pattern.compile(tokenTipo.patron);
-                Matcher matcher = patron.matcher(expresion.substring((posicionCaracter-1),posicionCaracter));
-                if(matcher.find()) {
+                Matcher matcher = patron.matcher(expresion.substring((posicionCaracter - 1), posicionCaracter));
+                if (matcher.find()) {
                     caracterValido = true;
                 }
             }
-                if(!caracterValido){
-                if(expresion.substring((posicionCaracter-1),posicionCaracter).equals(DECIMAL)){
-                AnalizarSecuencia(expresion);
-                }else{
-                    setCaracterInvalido("Error en el caracter "+expresion.substring((posicionCaracter-1),posicionCaracter));
+            if (!caracterValido) {
+                if (expresion.substring((posicionCaracter - 1), posicionCaracter).equals(DECIMAL)) {
+                    AnalizarSecuencia(expresion);
+                } else {
+                    setCaracterInvalido("Error en el caracter " + expresion.substring((posicionCaracter - 1), posicionCaracter));
                 }
                 return caracterValido;
             }
-            }
-            return caracterValido;
+        }
+        return caracterValido;
     }
+
     public void setCaracterInvalido(String caracterInvalido) {
         this.Error = caracterInvalido;
     }
-    public void AnalizarSecuencia(String Expresion){
-    final String regex = "[\\d.]+";
-    Pattern pattern = Pattern.compile(regex);
-    Matcher matcher = pattern.matcher(Expresion);
+    /**
+     * Metodo que busca el error lexico en secuencia
+     * @param Expresion valor que contiene una secuencia lexica no valida
+     */
+    public void AnalizarSecuencia(String Expresion) {
+        final String regex = "[\\d.]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(Expresion);
 
-while (matcher.find()) {
-    setCaracterInvalido("Error en la secuencia "+matcher.group());
-    break;
-}
-}
+        while (matcher.find()) {
+            setCaracterInvalido("Error en la secuencia " + matcher.group());
+            break;
+        }
+    }
 
     public String getCaracterInvalido() {
         return Error;
     }
 
-    
 }
